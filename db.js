f95c4d456b85acda039c23d3f767984141380d8f
Glitch (ephemerautomata) <none>
2019-10-08 20:09:14 +0000

    added data files to github

--- db.js
+++ db.js
@@ -3,20 +3,19 @@ const Datastore = require('nedb')
 
 module.exports = {
 	items: new Datastore({
-		filename: '.data/items.db',
+		filename: './data/items.db',
 		autoload: true
 	}),
 	ephemera: new Datastore({
-		filename: '.data/ephemera.db',
+		filename: './data/ephemera.db',
 		autoload: true
 	}),
 	skills: new Datastore({
-		filename: '.data/skills.db',
+		filename: './data/skills.db',
 		autoload: true
 	}),
 	players: new Datastore({
-		filename: '.data/players.db',
+		filename: './data/players.db',
 		autoload: true
 	})
 }
-
